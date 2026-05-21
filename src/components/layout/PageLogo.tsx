6bbb307b5a46fe2bfc1e31dfaad4cade986ceab8
Richard Herman <hermanrisa88@gmail.com>
2026-05-21 12:12:49 +0200

    feat/upravinamainpage

--- src/components/layout/PageLogo.tsx
+++ src/components/layout/PageLogo.tsx
@@ -6,5 +6,9 @@ import { useTranslations } from "next-intl";
 export function PageLogo() {
   const t = useTranslations();
 
-  return <Image src="/blogic-logo.png" alt={t("common.pageLogo.ariaLabel")} width={115} height={46} />;
+  return (
+    <a href="https://www.blogic.cz/" target="_blank" rel="noopener noreferrer" style={{ display: "inline-block" }}>
+      <Image src="/blogic-logo.png" alt={t("common.pageLogo.ariaLabel")} width={115} height={46} />
+    </a>
+  );
 }
